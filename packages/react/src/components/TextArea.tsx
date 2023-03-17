c4887387c47e4b28fe605fda4cb8d65f1a4a044f
rogerrm95 <rogerrm@live.com>
2023-03-16 21:45:47 -0300

    Componentes do Design System - Componente: TextArea

---
+++ packages/react/src/components/TextArea.tsx
@@ -0,0 +1,35 @@
+import { ComponentProps } from 'react'
+import { styled } from '../styles'
+
+export const TextArea = styled('textarea', {
+  background: '$gray900',
+  padding: '$3 $4',
+  borderRadius: '$sm',
+  boxSizing: 'border-box',
+  border: '2px solid $gray-900',
+
+  fontFamily: '$default',
+  fontSize: '$sm',
+  fontWeight: '$regular',
+  color: '$white',
+  resize: 'vertical',
+
+  minHeight: 80,
+  maxHeight: 400,
+
+  '&:focus': {
+    borderColor: '$ignite300',
+    outline: 'none',
+  },
+
+  '&:disabled': {
+    opacity: 0.5,
+    cursor: 'not-allowed',
+  },
+
+  '&:placeholder': {
+    color: '$gray400',
+  },
+})
+
+export interface TextAreaProps extends ComponentProps<typeof TextArea> {}
